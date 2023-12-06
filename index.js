74538ebcfa6c5ac2c5a19ae7a64e4776ef1cdc87
Goga Pipia <tskali0zamboni@gmail.com>
2023-12-06 17:50:30 -0500

    starter

--- index.js
+++ index.js
@@ -1,50 +1,132 @@
-// Complete the Numbers class below
-// the constructor has already been provided
-class Numbers {
-    constructor(data) {
-      //data can either be a string or an array of numbers
-      if (typeof data === "string") {
-        this.data = str.split(",").map((number) => number * 1);
-      } else {
-        this.data = data;
-      }
-    }
-    count() {
-      //return the count of numbers in data
-    }
-    printNumbers() {
-      //print the numbers in data
-    }
-    odds() {
-      //return the odd numbers in data
-    }
-    evens() {
-      //return the even numbers in data
-    }
-    sum() {
-      //return the sum of the numbers
-    }
-    product() {
-      //return the product of the numbers
-    }
-    greaterThan(target) {
-      //return the numbers greater than the target
-    }
-    howMany(target) {
-      //return the count of a given number
-    }
+const items = [
+    { id: 1, name: "apple", price: 1.75, categoryId: 1, inventory: 100 },
+    { id: 2, name: "banana", price: 0.25, categoryId: 1, inventory: 100 },
+    { id: 3, name: "orange", price: 1.0, categoryId: 1, inventory: 100 },
+    { id: 4, name: "broccoli", price: 3.0, categoryId: 2, inventory: 100 },
+    { id: 5, name: "cucumber", price: 1.0, categoryId: 2, inventory: 100 },
+    { id: 6, name: "milk", price: 5.75, categoryId: 3, inventory: 100 },
+    { id: 7, name: "cheddar cheese", price: 4.0, categoryId: 3, inventory: 100 },
+    { id: 8, name: "sourdough loaf", price: 5.5, categoryId: 4, inventory: 100 },
+  ];
+  
+  const cart = [];
+  
+  // ------------------ Complete the functions written below ------------------------------ //
+  
+  function logItemNames() {
+    //TODO: use the .forEach() method to log out the name of each item
   }
   
-  //Prompt the user for a list of integers separated by commas
-  const str = prompt("enter some numbers, like this", "1,2,3,3,5,9");
+  /**
+   * @param {number} id
+   * @returns {{id: number, name: string, price: number, category: string, inventory: number}} item
+   */
+  function findItemById(id) {
+    // TODO: Use the .find() method to return the item who's id matches the passed in id
+  }
   
-  //create an instance of numbers
-  const n1 = new Numbers(str);
-  console.log(n1.count()); //returns count of numbers
-  n1.printNumbers(); //prints the number along with their indexes
-  console.log(n1.odds()); //returns odd numbers
-  console.log(n1.evens()); //returns even numbers
-  console.log(n1.sum()); //returns sum of numbers
-  console.log(n1.product()); //returns product of numbers
-  console.log(n1.greaterThan(3)); //returns numbers greater than another number
-  console.log(n1.howMany(3)); //return the count of a specific number
+  /**
+   * @returns {items[]} Returns a new array with capitalized names
+   */
+  function capitalizeNames() {
+    // TODO:  Use the .map() and possibly .slice() methods and return a new items array with the item names capitalized
+    // DO NOT MUTATE THE ORIGINAL ARRAY IN YOU LOGIC
+  }
+  
+  /**
+   * @returns {number} the sum of all inventory items
+   */
+  
+  function calculateTotalInventory() {
+    // TODO Use the .reduce() method to return the total number of items in inventory
+  }
+  
+  /**
+   * @returns {number} the total price of all inventory items combined
+   */
+  function calculateAllInventoryPrice() {
+    // TODO Use the .reduce() method to return the total price of all the items in inventory
+  }
+  
+  /**
+   * @param {string} name
+   * @returns {number} the price of the item passed in
+   */
+  function getItemPriceByName(name) {
+    // TODO: Use your knowledge of objects and arrays to get the price of the item passed in
+  }
+  
+  /**
+   * @param {categoryId} id of category to find
+   * @returns {items[]} array of all items which belong to the given category
+   */
+  function filterItemsByCategoryId(categoryId) {
+    // TODO: use the .filter() method to filter out all items which don't belong the passed in category
+  }
+  
+  function logCartItems() {
+    // TODO: Loop through your cart and use the indexes to log the names of all items in your cart
+  }
+  
+  /**
+   * @returns { number } returns the total price of items in your cart
+   */
+  function calculateTotalCartPrice() {
+    // TODO: Loop through your cart and return the total price of all items in your cart
+  }
+  
+  // --------------------- DO NOT CHANGE THE CODE BELOW ------------------------ //
+  
+  const ids = prompt(
+    "enter numbers separated by commas for the ids of the items you want to add to your cart",
+    "1, 3, 5"
+  );
+  // Split the string of numbers into an array of strings.
+  const idArr = ids.split(", ");
+  
+  idArr.forEach((id) => cart.push(id));
+  console.log(`The names of all the items are: `);
+  logItemNames();
+  const itemId = prompt("enter the id of an item you are trying to find", "1");
+  console.log(
+    `The item with id ${itemId} is  ${JSON.stringify(
+      findItemById(+itemId),
+      null,
+      2
+    )}`
+  );
+  console.log(
+    "We can map over an array and return a new array with the names capitalized like so: ",
+    capitalizeNames()
+  );
+  console.log(
+    "The total inventory of all grocery items is: ",
+    calculateTotalInventory()
+  );
+  console.log(
+    "The total price of all items in inventory is: ",
+    calculateAllInventoryPrice()
+  );
+  
+  const itemToFind = prompt(
+    "Enter the name of an item to find the price of",
+    "apple"
+  );
+  console.log(`The price of ${itemToFind} is: `, getItemPriceByName(itemToFind));
+  
+  const categoryId = prompt(
+    "Enter a number between 1-4 to filter only items with that categoryId",
+    2
+  );
+  console.log(
+    `The items in category ${categoryId} are: `,
+    filterItemsByCategoryId(+categoryId)
+  );
+  
+  console.log("Cart items: ");
+  logCartItems();
+  
+  console.log(
+    `The total price of the items in your cart is: `,
+    calculateTotalCartPrice()
+  );
